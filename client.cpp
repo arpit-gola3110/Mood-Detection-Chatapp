466055f3924390eca76f7fc0e18748f35ad8098c
Arpit Gola <arpit31102006@gmail.com>
2024-11-03 01:41:18 +0530

    third update

--- client.cpp
+++ client.cpp
@@ -1,8 +1,11 @@
 #include <winsock2.h>
 #include <windows.h>
+#include <richedit.h>
 #include <string>
+#include <sstream>
 
 #pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "Msftedit.lib")
 
 #define ID_EDIT 101
 #define ID_BUTTON 102
@@ -14,39 +17,57 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     static HWND hEdit;
     static HWND hButton;
     static HWND hChat;
+    HFONT hFont;
 
     switch (uMsg) {
         case WM_CREATE: {
-            hChat = CreateWindowEx(0, "EDIT", "",
+            hFont = CreateFontA(
+                16, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE,
+                ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
+                DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, "Arial");
+
+            hChat = CreateWindowExA(WS_EX_CLIENTEDGE, "EDIT", "",
                                    WS_CHILD | WS_VISIBLE | WS_BORDER | ES_MULTILINE | ES_AUTOVSCROLL | ES_READONLY,
-                                   10, 10, 400, 200, hwnd, (HMENU)ID_CHAT, GetModuleHandle(NULL), NULL);
-            hEdit = CreateWindowEx(0, "EDIT", "",
+                                   10, 10, 420, 300, hwnd, (HMENU)ID_CHAT, GetModuleHandle(NULL), NULL);
+            SendMessageA(hChat, WM_SETFONT, (WPARAM)hFont, TRUE);
+
+            hEdit = CreateWindowExA(WS_EX_CLIENTEDGE, "EDIT", "",
                                    WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL,
-                                   10, 220, 300, 25, hwnd, (HMENU)ID_EDIT, GetModuleHandle(NULL), NULL);
-            hButton = CreateWindowEx(0, "BUTTON", "Send",
+                                   10, 320, 320, 25, hwnd, (HMENU)ID_EDIT, GetModuleHandle(NULL), NULL);
+            SendMessageA(hEdit, WM_SETFONT, (WPARAM)hFont, TRUE);
+
+            hButton = CreateWindowExA(0, "BUTTON", "Send",
                                      WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                                     320, 220, 90, 25, hwnd, (HMENU)ID_BUTTON, GetModuleHandle(NULL), NULL);
+                                     340, 320, 90, 25, hwnd, (HMENU)ID_BUTTON, GetModuleHandle(NULL), NULL);
+            SendMessageA(hButton, WM_SETFONT, (WPARAM)hFont, TRUE);
+
+            // Add a menu bar
+            HMENU hMenu = CreateMenu();
+            AppendMenuA(hMenu, MF_STRING, 1, "File");
+            AppendMenuA(hMenu, MF_STRING, 2, "Help");
+            SetMenu(hwnd, hMenu);
+
             break;
         }
         case WM_COMMAND: {
             if (LOWORD(wParam) == ID_BUTTON) {
                 char buffer[1024];
-                GetWindowText(hEdit, buffer, sizeof(buffer));
+                GetWindowTextA(hEdit, buffer, sizeof(buffer));
                 if (strlen(buffer) > 0) {
                     send(clientSocket, buffer, strlen(buffer), 0);
 
                     // Clear the input box
-                    SetWindowText(hEdit, "");
+                    SetWindowTextA(hEdit, "");
 
                     // Receive server response
                     char recvBuffer[1024] = {0};
                     recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
 
                     // Append the response to the chat window
-                    int len = GetWindowTextLength(hChat);
-                    SendMessage(hChat, EM_SETSEL, len, len);
-                    SendMessage(hChat, EM_REPLACESEL, 0, (LPARAM)recvBuffer);
-                    SendMessage(hChat, EM_REPLACESEL, 0, (LPARAM)"\r\n");
+                    int len = GetWindowTextLengthA(hChat);
+                    SendMessageA(hChat, EM_SETSEL, len, len);
+                    SendMessageA(hChat, EM_REPLACESEL, 0, (LPARAM)recvBuffer);
+                    SendMessageA(hChat, EM_REPLACESEL, 0, (LPARAM)"\r\n");
                 }
             }
             break;
@@ -75,19 +96,20 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
     const char CLASS_NAME[] = "ChatClient";
 
-    WNDCLASS wc = {};
+    WNDCLASSA wc = {};
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
 
-    RegisterClass(&wc);
+    RegisterClassA(&wc);
 
-    HWND hwnd = CreateWindowEx(
+    HWND hwnd = CreateWindowExA(
         0,
         CLASS_NAME,
         "Chat Client",
         WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 450, 300,
+        CW_USEDEFAULT, CW_USEDEFAULT, 450, 400,
         NULL,
         NULL,
         hInstance,
@@ -99,6 +121,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     }
 
     ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
 
     MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0)) {
@@ -107,8 +130,4 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     }
 
     closesocket(clientSocket);
-    WSACleanup();
-
-    return 0;
-}
-
+    }
